cbcd212753aaa33000edc2daa75b56bd7664acce
EdieAprovita <edieveg316@gmail.com>
2021-07-22 13:31:45 -0500

    Setup project

---
+++ webpack.config.dev.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizarPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+	mode: 'production',
+	entry: {
+		index: './src/index.js',
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+		clean: true,
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+		alias: {
+			'@components': path.resolve(__dirname, 'src/components/'),
+			'@styles': path.resolve(__dirname, 'src/styles/'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [{ loader: 'html-loader' }],
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [new CssMinimizarPlugin(), new TerserPlugin()],
+	},
+};
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		index: './src/index.js',
+	},
+	output: {
+		filename: 'main.bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [{ loader: 'html-loader' }],
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 5000,
+	},
+};
